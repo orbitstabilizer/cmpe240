abd35828983b91b9edac8d233da8b25f1ffad1df
orbitstabilizer <71184281+orbitstabilizer@users.noreply.github.com>
2023-05-04 18:01:16 +0300

    Refactoring

--- prelab4/source.v
+++ prelab4/source.v
@@ -4,27 +4,25 @@ module source(memory, instruction, r1, r2, r3, clk);
 
 input wire [15:0] instruction;
 input wire clk;
-parameter 	S0 = 4'b0000,
-			S1 = 4'b0001,
-			S2 = 4'b0010,
-			S3 = 4'b0011,
-			S4 = 4'b0100,
-			S5 = 4'b1000,
-			S6 = 4'b1001,
-			S7 = 4'b1010,
-            S8 = 4'b1011,
-            S9 = 4'b1100,
-            S10 = 4'b1101,
-            S11 = 4'b1110,
-            S12 = 4'b1111;
+parameter 	S0  = 4'b0000,  //0000 r3 = instruction[11:4]  S0
+            S1  = 4'b0001,  //0001 r3 = r1 == r2           S1
+            S2  = 4'b0010,  //0010 r3 = r1 < r2            S2
+            S3  = 4'b0011,  //0011 r3 = r1 <= r2           S3
+            S4  = 4'b0100,  //0100 r3 = r1 | r2 (or)       S4
+            S5  = 4'b1000,  //1000 r3 = r1 & r2 (and)      S5
+            S6  = 4'b1001,  //1001 r3 = r1 ^ r2 (xor)      S6
+            S7  = 4'b1010,  //1010 r3 = ~r1 (r2 is unused) S7
+            S8  = 4'b1011,  //1011 r3 = r1 + r2            S8
+            S9  = 4'b1100,  //1100 r3 = r1 - r2            S9
+            S10 = 4'b1101,  //1101 r3 = r1 * r2            S10
+            S11 = 4'b1110,  //1110 r3 = r1 / r2            S11
+            S12 = 4'b1111;  //1111 r3 = r1 % r2            S12
 
 integer i;
 integer s;
 		
 output reg [127:0] memory;
-output reg [7:0]r1;
-output reg [7:0]r2;
-output reg [7:0]r3;
+output reg [7:0]r1,r2,r3;
 
 initial begin
 	memory = 0;
@@ -46,123 +44,111 @@ always@(posedge clk) begin
 		r2[s] = memory[i];
 		s = s + 1;
 	end
-	//0000 r3 = instruction[11:4]  S0
-    //0001 r3 = r1 == r2           S1
-    //0010 r3 = r1 < r2            S2
-    //0011 r3 = r1 <= r2           S3
-    //0100 r3 = r1 | r2 (or)       S4
-    //1000 r3 = r1 & r2 (and)      S5
-    //1001 r3 = r1 ^ r2 (xor)      S6
-    //1010 r3 = ~r1 (r2 is unused) S7
-    //1011 r3 = r1 + r2            S8
-    //1100 r3 = r1 - r2            S9
-    //1101 r3 = r1 * r2            S10
-    //1110 r3 = r1 / r2            S11
-    //1111 r3 = r1 % r2            S12
+
 	case(instruction[15:12])
-		S0: begin
-			s = 0;
-			for (i = instruction[3:0] * 8; i < (instruction[3:0] * 8) + 8; i++) begin
-				memory[i] = instruction[11:4] >> s;
-				s = s + 1;
-			end 
-		end
-		S1: begin
-            r3 = r1 == r2;
-			s = 0;
-			for (i = instruction[3:0] * 8; i < (instruction[3:0] * 8) + 8; i++) begin
-				memory[i] = r3[s];
-				s = s + 1;
-			end 
-		end
-		S2: begin
-            r3 = r1 < r2;
-			s = 0;
-			for (i = instruction[3:0] * 8; i < (instruction[3:0] * 8) + 8; i++) begin
-				memory[i] = r3[s];
-				s = s + 1;
-			end 
-		end
-		S3: begin
-            r3 = r1 <= r2;
-			s = 0;
-			for (i = instruction[3:0] * 8; i < (instruction[3:0] * 8) + 8; i++) begin
-				memory[i] = r3[s];
-				s = s + 1;
-			end 
-		end
-		S4: begin
-            r3 = r1 | r2;
-			s = 0;
-			for (i = instruction[3:0] * 8; i < (instruction[3:0] * 8) + 8; i++) begin
-				memory[i] = r3[s];
-				s = s + 1;
-			end 
-		end
-		S5: begin
-			r3 = r1 & r2;
-			s = 0;
-			for (i = instruction[3:0] * 8; i < (instruction[3:0] * 8) + 8; i++) begin
-				memory[i] = r3[s];
-				s = s + 1;
-			end 
-		end
-		S6: begin
-			r3 = r1 ^ r2;
-			s = 0;
-			for (i = instruction[3:0] * 8; i < (instruction[3:0] * 8) + 8; i++) begin
-				memory[i] = r3[s];
-				s = s + 1;
-			end 
-		end
-		S7: begin
-            r3 = ~r1;
-            s = 0;
-            for (i = instruction[3:0] * 8; i < (instruction[3:0] * 8) + 8; i++) begin
-                memory[i] = r3[s];
-                s = s + 1;
-            end
-		end
-        S8: begin
-            r3 = r1 + r2;
-            s = 0;
-            for (i = instruction[3:0] * 8; i < (instruction[3:0] * 8) + 8; i++) begin
-                memory[i] = r3[s];
-                s = s + 1;
-            end
-        end
-        S9: begin
-            r3 = r1 - r2;
-            s = 0;
-            for (i = instruction[3:0] * 8; i < (instruction[3:0] * 8) + 8; i++) begin
-                memory[i] = r3[s];
-                s = s + 1;
-            end
-        end
-        S10: begin
-            r3 = r1 * r2;
-            s = 0;
-            for (i = instruction[3:0] * 8; i < (instruction[3:0] * 8) + 8; i++) begin
-                memory[i] = r3[s];
-                s = s + 1;
-            end
-        end
-        S11: begin
-            r3 = r1 / r2;
-            s = 0;
-            for (i = instruction[3:0] * 8; i < (instruction[3:0] * 8) + 8; i++) begin
-                memory[i] = r3[s];
-                s = s + 1;
-            end
-        end
-        S12: begin
-            r3 = r1 % r2;
-            s = 0;
-            for (i = instruction[3:0] * 8; i < (instruction[3:0] * 8) + 8; i++) begin
-                memory[i] = r3[s];
-                s = s + 1;
-            end
-        end
+    S0: begin //0000 r3 = instruction[11:4]  S0
+      s = 0;
+      for (i = instruction[3:0] * 8; i < (instruction[3:0] * 8) + 8; i++) begin
+        memory[i] = instruction[11:4] >> s; // LSB
+        s = s + 1;
+      end 
+    end
+    S1: begin //0001 r3 = r1 == r2           S1
+      r3 = r1 == r2;
+      s = 0;
+      for (i = instruction[3:0] * 8; i < (instruction[3:0] * 8) + 8; i++) begin
+        memory[i] = r3[s];
+        s = s + 1;
+      end 
+    end
+    S2: begin //0010 r3 = r1 < r2            S2
+      r3 = r1 < r2;
+      s = 0;
+      for (i = instruction[3:0] * 8; i < (instruction[3:0] * 8) + 8; i++) begin
+        memory[i] = r3[s];
+        s = s + 1;
+      end 
+    end
+    S3: begin //0011 r3 = r1 <= r2           S3
+      r3 = r1 <= r2;
+      s = 0;
+      for (i = instruction[3:0] * 8; i < (instruction[3:0] * 8) + 8; i++) begin
+        memory[i] = r3[s];
+        s = s + 1;
+      end 
+    end
+    S4: begin //0100 r3 = r1 | r2 (or)       S4
+      r3 = r1 | r2;
+      s = 0;
+      for (i = instruction[3:0] * 8; i < (instruction[3:0] * 8) + 8; i++) begin
+        memory[i] = r3[s];
+        s = s + 1;
+      end 
+    end
+    S5: begin //1000 r3 = r1 & r2 (and)      S5
+      r3 = r1 & r2;
+      s = 0;
+      for (i = instruction[3:0] * 8; i < (instruction[3:0] * 8) + 8; i++) begin
+        memory[i] = r3[s];
+        s = s + 1;
+      end 
+    end
+    S6: begin //1001 r3 = r1 ^ r2 (xor)      S6
+      r3 = r1 ^ r2;
+      s = 0;
+      for (i = instruction[3:0] * 8; i < (instruction[3:0] * 8) + 8; i++) begin
+        memory[i] = r3[s];
+        s = s + 1;
+      end 
+    end
+    S7: begin //1010 r3 = ~r1 (r2 is unused) S7
+      r3 = ~r1;
+      s = 0;
+      for (i = instruction[3:0] * 8; i < (instruction[3:0] * 8) + 8; i++) begin
+        memory[i] = r3[s];
+        s = s + 1;
+      end
+    end
+    S8: begin //1011 r3 = r1 + r2            S8
+      r3 = r1 + r2;
+      s = 0;
+      for (i = instruction[3:0] * 8; i < (instruction[3:0] * 8) + 8; i++) begin
+        memory[i] = r3[s];
+        s = s + 1;
+      end
+    end
+    S9: begin //1100 r3 = r1 - r2            S9
+      r3 = r1 - r2;
+      s = 0;
+      for (i = instruction[3:0] * 8; i < (instruction[3:0] * 8) + 8; i++) begin
+        memory[i] = r3[s];
+        s = s + 1;
+      end
+    end
+    S10: begin //1101 r3 = r1 * r2            S10
+      r3 = r1 * r2;
+      s = 0;
+      for (i = instruction[3:0] * 8; i < (instruction[3:0] * 8) + 8; i++) begin
+        memory[i] = r3[s];
+        s = s + 1;
+      end
+    end
+    S11: begin //1110 r3 = r1 / r2            S11
+      r3 = r1 / r2;
+      s = 0;
+      for (i = instruction[3:0] * 8; i < (instruction[3:0] * 8) + 8; i++) begin
+        memory[i] = r3[s];
+        s = s + 1;
+      end
+    end
+    S12: begin //1111 r3 = r1 % r2            S12
+      r3 = r1 % r2;
+      s = 0;
+      for (i = instruction[3:0] * 8; i < (instruction[3:0] * 8) + 8; i++) begin
+        memory[i] = r3[s];
+        s = s + 1;
+      end
+    end
 	endcase
 end
 
--- prelab4/testbench.v
+++ prelab4/testbench.v
@@ -1,9 +1,7 @@
 `timescale 1ns/1ns
 module testbench();
 wire [127:0] memory;
-wire [7:0] r1;
-wire [7:0] r2;
-wire [7:0] r3;
+wire [7:0] r1,r2,r3;
 reg [15:0] instruction;
 reg clk;
 integer i;
@@ -11,31 +9,24 @@ integer i;
 source s(memory, instruction, r1, r2, r3, clk);
 
 initial begin
-    $dumpfile("TimingDiagram.vcd");
-    $dumpvars(0, memory, instruction, r1, r2, r3, clk);
-	/*
-	000 operation code
-	000 r1
-	000 r2
-	000 r3
-	r3(memory[0]) = r1(memory[3]) + r2(memory[7])
-	*/
-    instruction = 16'h0020;
-	#20;
-	instruction = 16'h0031;
-	#20;
-	instruction = 16'hD012;
-	#20;
-    
-    
-    $finish;
+  $dumpfile("TimingDiagram.vcd");
+  $dumpvars(0, memory, instruction, r1, r2, r3, clk);
+  instruction = 16'h0020;
+  #20;
+  instruction = 16'h0031;
+  #20;
+  instruction = 16'hD012;
+  #20;
+
+
+  $finish;
 end
 
 always begin	
-	clk = 1;
-	#10;
-	clk = 0;
-	#10;
+  clk = 1;
+  #10;
+  clk = 0;
+  #10;
 end
 
 endmodule
